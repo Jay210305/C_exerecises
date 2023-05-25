596c0e3745ad142cd1737c6efca25501047df281
Jay210305 <94190939+Jay210305@users.noreply.github.com>
2023-05-25 10:07:27 -0500

    Add files via upload

--- actividad 1 sesion 8.cpp
+++ actividad 1 sesion 8.cpp
@@ -7,7 +7,7 @@ class Figura
 public:
     virtual void calcular_area() = 0;
 
-    ~Figura(){}
+    virtual ~Figura() {}
 };
 
 class Triangulo : public Figura
@@ -20,11 +20,11 @@ public:
     Triangulo(float base, float altura) : base(base), altura(altura) {}
     void calcular_area() override
     {
-        int area = (base * altura) / 2;
-        cout << "El área de la figura es: " << area << endl;
+        float area = (base * altura) / 2.0;
+        cout << "El area de la figura es: " << area << endl;
     }
 
-    ~Triangulo(){}
+    ~Triangulo() {}
 };
 
 class Rectangulo : public Figura
@@ -37,11 +37,11 @@ public:
     Rectangulo(float base, float altura) : base(base), altura(altura) {}
     void calcular_area() override
     {
-        int area = (base * altura);
-        cout << "El área de la figura es: " << area << endl;
+        float area = base * altura;
+        cout << "El area de la figura es: " << area << endl;
     }
 
-    ~Rectangulo(){}
+    ~Rectangulo() {}
 };
 
 class Circulo : public Figura
@@ -53,11 +53,44 @@ public:
     Circulo(float radio) : radio(radio) {}
     void calcular_area() override
     {
-        float area = (M_PI * pow(radio, 2));
-        cout << "El área de la figura es: " << area << endl;
+        float area = M_PI * pow(radio, 2);
+        cout << "El area de la figura es: " << area << endl;
     }
 
-    ~Circulo(){}
+    ~Circulo() {}
+};
+
+class Pentagono : public Figura
+{
+private:
+    float apotema;
+    float lado;
+
+public:
+    Pentagono(float apotema, float lado) : apotema(apotema), lado(lado) {}
+    void calcular_area() override
+    {
+        float area = (5*lado*apotema)/2;
+        cout << "El area de la figura es: " << area << endl;
+    }
+
+    ~Pentagono() {}
+};
+
+class Cuadrado : public Figura
+{
+private:
+    float lado;
+
+public:
+    Cuadrado(float lado) : lado(lado) {}
+    void calcular_area() override
+    {
+        float area = pow(lado,2);
+        cout << "El area de la figura es: " << area << endl;
+    }
+
+    ~Cuadrado() {}
 };
 
 int main()
@@ -66,18 +99,20 @@ int main()
     float base;
     float altura;
     float radio;
+    float apotema;
+    float lado;
     do
     {
         cout << "Menu" << endl;
-        cout << "Escoja una figura para calcular su area";
-        cout << "1 => Triangulo\n2 => Rectangulo\n3 => Circulo\n4 => Salir" << endl;
-        cout << "Ingrese una opcion";
+        cout << "Escoja una figura para calcular su area" << endl;
+        cout << "1 => Triangulo\n2 => Rectangulo\n3 => Circulo\n4 => Pentagono\n5 => Cuadrado\n6 => Salir" << endl;
+        cout << "Ingrese una opcion: ";
         cin >> opcion;
 
-        if (opcion < 1 || opcion > 4)
+        if (opcion < 1 || opcion > 6)
         {
-        cout << "Opcion invalida. Intente de nuevo." << endl;
-        continue;
+            cout << "Opcion invalida. Intente de nuevo." << endl;
+            continue;
         }
 
         switch (opcion)
@@ -87,8 +122,10 @@ int main()
             cin >> base;
             cout << "Ingrese la longitud de la altura: ";
             cin >> altura;
-            Triangulo triangulo (base,altura);
-            triangulo.calcular_area();
+            {
+                Triangulo triangulo(base, altura);
+                triangulo.calcular_area();
+            }
             break;
 
         case 2:
@@ -96,18 +133,43 @@ int main()
             cin >> base;
             cout << "Ingrese la longitud de la altura: ";
             cin >> altura;
-            Rectangulo rectangulo (base,altura);
-            rectangulo.calcular_area();
+            {
+                Rectangulo rectangulo(base, altura);
+                rectangulo.calcular_area();
+            }
             break;
-        
+
         case 3:
             cout << "Ingrese el radio del círculo: ";
             cin >> radio;
-            Circulo circulo (radio);
-            circulo.calcular_area();
+            {
+                Circulo circulo(radio);
+                circulo.calcular_area();
+            }
+            break;
+
+        case 4:
+            cout << "Ingrese la longitud de la apotema: ";
+            cin >> apotema;
+            cout << "Ingrese la longitud de yun lado de la figura: ";
+            cin >> lado;
+            {
+                Pentagono pentagono(apotema, lado);
+                pentagono.calcular_area();
+            }
+            break;
+
+        case 5:
+            cout << "Ingrese la longitud de un lado: ";
+            cin >> lado;
+            {
+                Cuadrado cuadrado(lado);
+                cuadrado.calcular_area();
+            }
             break;
         }
-    } while (opcion != 4);
-    cout << "Hasta luego";
+    } while (opcion != 6);
+
+    cout << "Hasta luego" << endl;
     return 0;
 }
